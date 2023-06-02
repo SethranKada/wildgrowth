6d8a0cf3ffc79b3b869cf889e1e866dd1729bcbd
Caden Lee <CadenCLee@protonmail.com>
2023-06-02 18:00:06 +0000

    complete refactor (#6)

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-
-use cli;
-use tokio;
-
-#[tokio::main]
-async fn main() {
-    cli::begin_session().await;
-}
--- wildgrowth_cli/src/main.rs
+++ wildgrowth_cli/src/main.rs
@@ -1,39 +1,37 @@
 #![allow(unused)]
-mod commands;
-use commands as arguments;
-use api::start_instance;
+use wildgrowth_api::start_instance;
 
+use clap::{Parser, Subcommand, ValueEnum};
 use std::time::Duration;
-use tokio::{signal, select, io::stdin, time::sleep};
 use tokio::sync::mpsc::{channel, Sender};
+use tokio::{io::stdin, select, signal, time::sleep};
 use tokio_util::sync::CancellationToken;
-use clap::{Parser, Subcommand,ValueEnum};
 
-pub async fn begin_session() {
+mod arguments;
+
+#[tokio::main]
+async fn main() {
     let cli = arguments::Cli::parse();
     let token = CancellationToken::new();
     let (send, mut recv) = channel(1);
 
     println!("Creating tasks");
     for i in 0..10 {
-        tokio::spawn(start_instance(
-            i,
-            send.clone(),
-            token.child_token()));
+        tokio::spawn(start_instance(i, send.clone(), token.child_token()));
     }
 
     match signal::ctrl_c().await {
         Ok(()) => {
-            println!("");
+            println!();
             token.cancel();
             drop(send);
             let _ = recv.recv().await;
             println!("Done!");
             std::process::exit(exitcode::OK);
-        },
+        }
         Err(err) => {
             eprintln!("Unable to listen for shutdown signal: {}", err);
             std::process::exit(exitcode::OSERR);
-        },
+        }
     }
 }
